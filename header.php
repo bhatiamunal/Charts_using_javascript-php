3ddf7c316562a7e7b063de03bef6fe92b620fb88
bhatiamunal8987 <bhatiamunal8987@gmail.com>
2021-02-04 19:32:39 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HighCharts</title>
+    <script src="javascript/lib/highcharts.js"></script>
+    <script src="javascript/lib/modules/series-label.js"></script>
+    <script src="javascript/lib/modules/exporting.js"></script>
+    <script src="javascript/lib/modules/export-data.js"></script>
+    <script src="javascript/lib/modules/accessibility.js"></script>
+
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+include 'header.php';
+?>
+
+
+
+
+<figure class="highcharts-figure">
+  <div id="container"></div>
+  <p class="highcharts-description">
+    Basic line chart showing trends in a dataset. This chart includes the
+    <code>series-label</code> module, which adds a label to each line for
+    enhanced readability.
+  </p>
+</figure>
+<script src="javascript/charts/chart.js"></script>
